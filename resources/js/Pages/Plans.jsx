9f5a7d3210fbcbf75261a1ec5ff92bbcdaa029c2
Vinicius Florentino <vinicesar2912@gmail.com>
2024-02-03 18:13:54 -0300

    fix navigation layout plans

--- resources/js/Pages/Plans.jsx
+++ resources/js/Pages/Plans.jsx
@@ -1,36 +1,12 @@
 import React from "react";
 import { Head } from "@inertiajs/react";
-<<<<<<< Updated upstream
-import NavigationLayout from "@/Layouts/NavigationLayout";
-
-import Stack from "@mui/material/Stack";
-import OfferCard from "@/Components/cards/OfferCard";
-
-export default function Plans({ auth, plans }) {
-    return (
-        <NavigationLayout user={auth.user}>
-            <Head title="Planos" />
-
-            <Stack direction="row" spacing={0} useFlexGap flexWrap="wrap">
-                {plans?.map((plan) => (
-                    <OfferCard
-                        key={plan.id}
-                        name={plan.name}
-                        price={plan.price}
-                        description={plan.description}
-                        benefits={plan.benefits}
-                    />
-                ))}
-            </Stack>
-        </NavigationLayout>
-=======
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import OfferCard from "@/Components/cards/OfferCard";
+import NavigationLayout from "@/Layouts/NavigationLayout";
 export default function Plans({ plans, auth }) {
     return (
-        <AuthenticatedLayout user={auth.user}>
+        <NavigationLayout user={auth.user}>
             <Head title="Planos" />
             <Box noValidate sx={{ width: "100%", py: 2 }}>
                 <Stack direction="row" spacing={5} useFlexGap flexWrap="wrap" sx={{
@@ -47,7 +23,6 @@ export default function Plans({ plans, auth }) {
                     ))}
                 </Stack>
             </Box>
-        </AuthenticatedLayout>
->>>>>>> Stashed changes
+        </NavigationLayout>
     );
 }
