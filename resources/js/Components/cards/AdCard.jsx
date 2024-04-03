cc2b5f8021b4203eebe89475a325f0dbe1f26e47
Vinicius Florentino <vinicesar2912@gmail.com>
2024-04-03 01:59:53 -0300

    corrige adcard

--- resources/js/Components/cards/AdCard.jsx
+++ resources/js/Components/cards/AdCard.jsx
@@ -6,75 +6,76 @@ import Typography from "@mui/material/Typography";
 import formatPrice from "@/Utils/formatPrice";
 import RemixIcon from "../RemixIcon";
 import Image from "../Image";
-
+import { CardActionArea } from "@mui/material";
 export default function AdCard({ title, sellerName, imageSrc, price, href }) {
     return (
         <Card sx={{ width: "250px", maxHeight: "auto", borderRadius: "16px" }}>
-            <CardContent sx={{ p: 0 }}>
-                <Box
-                    sx={{
-                        p: 0,
-                        m: 0,
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Image
-                        style={{
-                            width: "100%",
-                            height: "170px",
-                            objectFit: "cover",
-                        }}
-                        src={imageSrc}
-                    />
-                </Box>
-                <Box
-                    noValidate
-                    sx={{
-                        px: 2,
-                    }}
-                >
-                    <Typography
+            <CardActionArea href={href}>
+                <CardContent sx={{ p: 0 }}>
+                    <Box
                         sx={{
-                            fontSize: 14,
-                            color: "var(--dark-color)",
-                        }}
-                    >
-                        {title}
-                    </Typography>
-                </Box>
-                <Box
-                    noValidate
-                    sx={{
-                        pl: 1.5,
-                    }}
-                >
-                    <Typography
-                        sx={{
-                            fontSize: 14,
-                            color: "var(--dark-color)",
-                            alignItems: "center",
+                            p: 0,
+                            m: 0,
                             display: "flex",
-                            gap: "5px",
+                            justifyContent: "center",
+                            alignItems: "center",
                         }}
                     >
-                        <RemixIcon className={"ri-user-2-line"} />
-                        {sellerName}
-                    </Typography>
-                </Box>
-                <Box></Box>
-                <Box
-                    noValidate
-                    sx={{
-                        px: 2,
-                    }}
-                >
-                    <Typography sx={{ fontSize: 18, fontWeight: 700 }}>
-                        {formatPrice(price)}
-                    </Typography>
-                </Box>
-            </CardContent>
+                        <Image
+                            style={{
+                                width: "100%",
+                                height: "170px",
+                                objectFit: "cover",
+                            }}
+                            src={imageSrc}
+                        />
+                    </Box>
+                    <Box
+                        noValidate
+                        sx={{
+                            px: 2,
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                fontSize: 14,
+                                color: "var(--dark-color)",
+                            }}
+                        >
+                            {title}
+                        </Typography>
+                    </Box>
+                    <Box
+                        noValidate
+                        sx={{
+                            pl: 1.5,
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                fontSize: 14,
+                                color: "var(--dark-color)",
+                                alignItems: "center",
+                                display: "flex",
+                                gap: "5px",
+                            }}
+                        >
+                            <RemixIcon className={"ri-user-2-line"} />
+                            {sellerName}
+                        </Typography>
+                    </Box>
+                    <Box
+                        noValidate
+                        sx={{
+                            px: 2,
+                        }}
+                    >
+                        <Typography sx={{ fontSize: 18, fontWeight: 700 }}>
+                            {formatPrice(price)}
+                        </Typography>
+                    </Box>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 }
