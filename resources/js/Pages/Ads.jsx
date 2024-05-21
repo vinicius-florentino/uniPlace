e1d47cb2494a14ce1355814b614f34c98bda5ed8
Marcos Felipe <marcosfelipe645@gmail.com>
2024-05-21 17:23:54 -0300

    filtro por categoria com multiselect

--- resources/js/Pages/Ads.jsx
+++ resources/js/Pages/Ads.jsx
@@ -1,24 +1,78 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NavigationLayout from "@/Layouts/NavigationLayout";
 import { Head, useForm, router } from "@inertiajs/react";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import Loading from "@/Components/Loading";
 import AdCard from "@/Components/cards/AdCard";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Pagination from "@mui/material/Pagination";
 import SearchField from "@/Layouts/NavigationLayout/Components/SearchField";
 import Alert from "@mui/material/Alert";
-import CheckBoxFilters from "../Components/CheckBoxFilters";
+import FormControl from "@mui/material/FormControl";
+import Checkbox from "@mui/material/Checkbox";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionActions from "@mui/material/AccordionActions";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import RemixIcon from "@/Components/RemixIcon";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import ListItemText from "@mui/material/ListItemText";
 
 export default function Ads({ ads, auth, adsCategories }) {
-    const searchParams = new URLSearchParams(window.location.search);    
+    
+    let paginationTotal = ads?.last_page;
+    let actualPage = ads?.current_page;
+    const searchParams = new URLSearchParams(window.location.search);
+    const [loading, setLoading] = useState(false);
 
-    const { data, setData, get, processing } = useForm({
-        search: searchParams.get("search") || "",
-        filters: { ads_categories: [] }
+    const defaultSearch = "";
+
+    const defaultFilters = {
+        ads_categories: [],
+    };
+
+    const filters = {};
+    searchParams.forEach((value, key) => {
+        if (key.startsWith("filters")) {
+            const filterKey = key.split("[")[1].split("]")[0];
+            if (!filters[filterKey]) {
+                filters[filterKey] = [];
+            }
+            filters[filterKey].push(parseInt(value));
+        }
     });
 
-    const [loading, setLoading] = useState(false);
+    const initialFilters =
+        Object.keys(filters).length > 0 ? filters : defaultFilters;
+
+    const { data, setData, get, processing } = useForm({
+        search: searchParams.get("search") || defaultSearch,
+        filters: initialFilters,
+    });
+
+    const [expanded, setExpanded] = useState(data.filters !== defaultFilters);
+
+    const handleRemoveFilters = () => {
+        setData("filters", defaultFilters);
+    };
+
+    const handleAccordionChange = (e, expanded) => {
+        setExpanded(expanded);
+    };
+
+    const handleFiltersChange = (e) => {
+        const { name, value } = e.target;
+        setData((prevData) => {
+            let newFilters = { ...prevData.filters };
+            newFilters[name] = value;
+            return { ...prevData, filters: newFilters };
+        });
+    };
 
     const handleSearchChange = (e) => {
         const { value } = e.target;
@@ -28,7 +82,7 @@ export default function Ads({ ads, auth, adsCategories }) {
     const handlePaginationChange = (e, page) => {
         setLoading(true);
         router.visit("/ads", {
-            data: { page, search: data.search },
+            data: { page, search: data.search, filters: data.filters },
             onFinish: () => setLoading(false),
         });
     };
@@ -42,10 +96,6 @@ export default function Ads({ ads, auth, adsCategories }) {
         });
     };
 
-    let paginationTotal = ads?.last_page;
-
-    let actualPage = ads?.current_page;
-
     return (
         <NavigationLayout user={auth.user}>
             <Head title="Anúncios" />
@@ -54,16 +104,118 @@ export default function Ads({ ads, auth, adsCategories }) {
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <SearchField
+                            onSubmit={onSubmit}
                             value={data.search}
                             onChange={handleSearchChange}
                             disabled={processing}
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <CheckBoxFilters
-                            adsCategories={adsCategories}
-                            setData={setData}
-                        />
+                        <Accordion
+                            sx={{ px: 2, py: 1 }}
+                            expanded={expanded}
+                            onChange={handleAccordionChange}
+                        >
+                            <AccordionSummary
+                                expandIcon={
+                                    <RemixIcon className="ri-arrow-down-s-line" />
+                                }
+                                aria-controls="panel2-content"
+                                id="panel2-header"
+                            >
+                                <Box sx={{ width: "100%" }}>
+                                    <Typography
+                                        sx={{
+                                            fontWeight: 500,
+                                            fontSize: 16,
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "start",
+                                            gap: "8px",
+                                        }}
+                                    >
+                                        <RemixIcon className="ri-filter-3-line" />
+                                        Filtros
+                                    </Typography>
+                                </Box>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} md={6}>
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-multiple-name-label">
+                                                Categorias
+                                            </InputLabel>
+                                            <Select
+                                                labelId="demo-multiple-name-label"
+                                                id="demo-multiple-name"
+                                                multiple
+                                                value={
+                                                    data.filters.ads_categories
+                                                }
+                                                name="ads_categories"
+                                                onChange={handleFiltersChange}
+                                                input={
+                                                    <OutlinedInput label="Categorias" />
+                                                }
+                                                renderValue={(selected) =>
+                                                    selected
+                                                        .map(
+                                                            (categoryId) =>
+                                                                adsCategories.find(
+                                                                    (
+                                                                        category
+                                                                    ) =>
+                                                                        category.id ===
+                                                                        categoryId
+                                                                )?.name || ""
+                                                        )
+                                                        .join(", ")
+                                                }
+                                            >
+                                                {adsCategories.map(
+                                                    (category, index) => (
+                                                        <MenuItem
+                                                            key={index}
+                                                            value={category.id}
+                                                        >
+                                                            <Checkbox
+                                                                checked={
+                                                                    data.filters.ads_categories.indexOf(
+                                                                        category.id
+                                                                    ) > -1
+                                                                }
+                                                            />
+                                                            <ListItemText
+                                                                primary={
+                                                                    category.name
+                                                                }
+                                                            />
+                                                        </MenuItem>
+                                                    )
+                                                )}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+                            </AccordionDetails>
+                            <AccordionActions>
+                                <Button
+                                    disableElevation
+                                    variant="containedLight"
+                                    onClick={() => handleRemoveFilters()}
+                                >
+                                    Remover filtros
+                                </Button>
+                                <Button
+                                    type="submit"
+                                    disableElevation
+                                    variant="contained"
+                                >
+                                    Aplicar
+                                </Button>
+                            </AccordionActions>
+                        </Accordion>
                     </Grid>
                 </Grid>
             </Box>
@@ -98,7 +250,9 @@ export default function Ads({ ads, auth, adsCategories }) {
                     </Box>
                     {ads.data.length === 0 && (
                         <Box sx={{ width: "100%" }}>
-                            <Alert severity="info">Nenhum anúncio foi encontrado</Alert>
+                            <Alert severity="info">
+                                Nenhum anúncio foi encontrado
+                            </Alert>
                         </Box>
                     )}
                     {ads.data.length >= 1 && (
