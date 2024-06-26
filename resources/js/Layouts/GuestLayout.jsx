54f571ed23a9d301c5488839aecebea1da3ee4b5
Marcos Felipe <marcosfelipe645@gmail.com>
2024-06-26 14:45:30 -0300

    centralizei as tela de auth p enganar

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,30 +3,41 @@ import Box from "@mui/system/Box";
 import Logo from "@/Components/Logo";
 import { router } from "@inertiajs/react";
 import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
 
 export default function Guest({ children }) {
     return (
-        <Box
-            component="section"
-            sx={{
-                // minHeight: "80vh",
-                width: "320px",
-                margin: "0 auto",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                p: 4,
-                my: 4,
-                boxShadow: "var(--box-shadow)",
-                backgroundColor: "var(--white-color)"
-            }}
-        >
-            <Box component={Link} onClick={() => router.visit("/")} sx={{ mb: 2 }}>
-                <Logo height="48px" width="48px" />
-            </Box>
+        <Container maxWidth="xs" disableGutters>
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "100vh",
+                    margin: "0 auto",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Box
+                    sx={{
+                        p: 4,
+                        boxShadow: "var(--box-shadow)",
+                        backgroundColor: "var(--white-color)",
+                    }}
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap="8px"
+                >
+                    <Box component={Link} onClick={() => router.visit("/")}>
+                        <Logo height="48px" width="48px" />
+                    </Box>
 
-            <div>{children}</div>
-        </Box>
+                    <Box>{children}</Box>
+                </Box>
+            </Box>
+        </Container>
     );
 }
